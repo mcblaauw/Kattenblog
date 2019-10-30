cb40dad2937799bd2e0a03b2f4ca811bbe52e622
Marten Blaauw <mcblaauw@gmail.com>
2019-10-30 22:17:16 +0100

    socket.io faker experiments

--- v1/app.js
+++ v1/app.js
@@ -1,36 +1,27 @@
 var express = require('express');
 var app = express();
+var bodyParser = require("body-parser");
+var faker = require("faker/locale/nl");
+//socket.io experiment (server-client realtime communication)
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 
-const faker = require("faker/locale/nl");
 app.set("view engine","ejs");
-const Cats = require('./models/cat.model'); 
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+const Cats = require('./models/cat.model');
 
-// new cat in database
-/*
-Smokey = {
-    name: "Smokey",
-    image: faker.image.cats()
-};
-*/
-
-//let Cat = new Cats(Smokey);
-
-/*
-Cat.save(function (err) {
-    if (err) return next(err);
-    console.log('Cat added to database');
-});
-*/
-
-// Routes
+// ----------- Routes -------------------
 app.get("/", function(req,res) {
-    var cats = faker.image.cats();
-    res.render("home",{cats: cats});
+    //Random cat for homepage
+    var acat = faker.image.cats();
+    res.render("home",{cats: acat});
 });
 
 app.get("/cats", function(req,res,next) {
+    // Import all cats from database
     Cats.find({}, function(err, cats) {
-        console.log(cats);
         if (err) {
             return next(err);
         } else {
@@ -39,12 +30,44 @@ app.get("/cats", function(req,res,next) {
     });
 });
 
+app.post("/cats/create", function (req,res) {
+    newCat = {name: req.body.name, image: req.body.image};
+    let Cat = new Cats(newCat);
+
+    Cat.save(function (err) {
+        if (err) return next(err);
+        console.log('Cat added to database');
+    });
+    
+    res.redirect("/cats");
+});
+
+app.get("/cats/create", function (req,res) {
+    //render to new cat page
+    res.render("cats/create");
+});
+
 app.get("*", function(req,res) {
     res.send("The requested website does not exist!");
 });
 
+// --------- Socket.io ------------------
+io.on('connection', function(socket) {
+    console.log('a user has connected');
+    socket.on('disconnect', function(){
+        console.log('a user has disconnected');
+    });
+    socket.on('cat-event', function() {
+        var caturl = faker.image.cats();
+        console.log('random cat image generated');
+        io.emit('message', caturl);
+    });
+});
+
+
+// ---------- set up port and broadcast ------------
 let port = 3000;
 
-app.listen(port,function(){
-	console.log("Connecting to new server on port: "+port);
+server.listen(port,function(){
+	console.log("App running on: "+port);
 });
