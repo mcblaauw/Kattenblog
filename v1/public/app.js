8d2ce395d33688ffbced2548acd8c4d3ef437944
Marten Blaauw <mcblaauw@gmail.com>
2019-10-30 22:17:56 +0100

    socket.io faker experiments

---
+++ v1/public/app.js
@@ -0,0 +1,7 @@
+$("button.GenCat").on('click',function(){
+    // Generate cat image 
+    // Put result in div element with class="randomcat"
+    // Give form button (name="image") the value of its URL
+
+    
+});
